d39045c05360e38c021fb403cd707a43b9d62556
Anas Attoum <anasattoum.12321@gmail.com>
2024-10-07 21:15:37 +0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+
+
 function App() {
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Header />}>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/constants/data.ts
@@ -0,0 +1,3 @@
+export const links = [
+    { name: 'Departments', url: '/' }
+]
